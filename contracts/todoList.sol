86e3565d408d8c980e28f19f675a33ecf199d368
bobbyrinaldo29 <bobbyrinaldo@icloud.com>
2026-04-21 11:12:52 +0700

    git init

---
+++ contracts/todoList.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract TodoList {
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    // setiap user punya list task masing-masing
+    mapping (address => Task[]) public userTasks;
+
+    event TaskCreated(address user, uint id, string content);
+    event TaskCompleted(address user, uint id);
+
+    function createTask(string memory _content) public {
+        uint taskId = userTasks[msg.sender].length;
+        userTasks[msg.sender].push(Task(taskId, _content, false));
+
+        emit TaskCreated(msg.sender, taskId, _content);
+    }
+
+    function toggleCompleted(uint _id) public {
+        Task storage task = userTasks[msg.sender][_id];
+        task.completed = !task.completed;
+
+        emit TaskCompleted(msg.sender, _id);
+    }
+
+    function getTaskCount() public view returns (uint) {
+        return userTasks[msg.sender].length;
+    }
+}
